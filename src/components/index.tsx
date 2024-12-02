9a0047a197d1dfd839b7e0f40f066c1e470f0c6f
Matt Rintoul <matt@sayyeah.com>
2024-12-02 10:37:11 -0500

    Update exports to include more components & presets

--- src/components/index.tsx
+++
@@ -1,5 +0,0 @@
-export { Button } from "./button/Button";
-export { Card } from "./card/Card";
-export { Group } from "./group/Group";
-export { Select } from "./select/Select";
-export { Svg } from "./svg/Svg";
