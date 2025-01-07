c833ea54e124f320ce58240db66548cbc32968a2
Matt Rintoul <matt@sayyeah.com>
2025-01-07 11:59:25 -0500

    Add type for onValueChange

--- dist/components/select/Select.d.ts
+++ dist/components/select/Select.d.ts
@@ -6,12 +6,13 @@ export type SelectProps = {
     options?: string[] | Option[];
     native?: boolean;
     icon?: string;
+    onValueChange: (value: string) => void;
     children?: React.ReactElement<{
         native: boolean;
     }>;
 } & WithVariants<"div", null, {
     "min-size"?: "none" | "sm" | "md" | "lg";
 }>;
-export declare const Select: React.ForwardRefExoticComponent<Omit<SelectProps, "ref"> & React.RefAttributes<unknown>> & {
+export declare const Select: React.ForwardRefExoticComponent<Omit<SelectProps, "ref"> & React.RefAttributes<HTMLDivElement>> & {
     Options: typeof SelectOptions;
 };
